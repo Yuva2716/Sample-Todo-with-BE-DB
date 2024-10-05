202cdb7c32f2f94fc1f43e35777a3d50e661f8ab
Yuva2716 <yuvarajagalya2716@outlook.com>
2024-10-05 21:28:17 +0530

    dotenv file added and used in code

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,26 @@ import express from "express"
 import cors from "cors"
 // importing mongodb
 import { MongoClient, ObjectId } from "mongodb"
+import dotenv from "dotenv"
 
+dotenv.config();
 
 // connecting with mongodb atlas
-const client = new MongoClient("mongodb+srv://yuvapraba2716:2716@cluster0.mbcun.mongodb.net/")
+const client = new MongoClient(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+})
 let todolists;
+
 const main = async()=>{
-    await client.connect()
-    console.log("connected with mongodb");
-    todolists = client.db("tododatabase").collection("todolists")
+    try {
+        await client.connect()
+        console.log("connected with mongodb");
+        todolists = client.db("tododatabase").collection("todolists")
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
     
 }
 
@@ -22,30 +33,47 @@ app.use(express.json())
 // CRUD Operations
 
 app.get("/", async (req, res) => {
-    const data = await todolists.find().toArray()
-    res.send({
+    try {
+        const data = await todolists.find().toArray()
+        res.send({
         status: 200,
         data
     })
+    } catch (error) {
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+    }
 })
 
 app.post("/", async(req, res) => {
-    let ipvalue = req.body.ipvalue
-    await todolists.insertOne({ipvalue})
-    res.status(200).json({ status: true })
+    try {
+        let ipvalue = req.body.ipvalue
+        await todolists.insertOne({ipvalue})
+        res.status(200).json({ status: true })
+    } catch (error) {
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+    }
 })
 
 app.put("/", async(req, res) => {
-    const { ipvalue } = req.body
-    const  index  = req.query.index
-    await todolists.updateOne({_id:new ObjectId(index)},{$set:{ipvalue}})
-    res.status(200).json({ status: true })
+    try {
+        const { ipvalue } = req.body
+        const  index  = req.query.index
+        await todolists.updateOne({_id:new ObjectId(index)},{$set:{ipvalue}})
+        res.status(200).json({ status: true })
+    } catch (error) {
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+    }
 })
 
 app.delete("/:index", async(req, res) => {
-    const { index } = req.params
-    await todolists.deleteOne({_id:new ObjectId(index)})
-    res.status(200).json({ status: true })
+    try {
+        const { index } = req.params
+        await todolists.deleteOne({_id:new ObjectId(index)})
+        res.status(200).json({ status: true })
+    } catch (error) {
+        res.status(500).send({ status: 500, message: "Internal Server Error" });
+    }
+    
 })
 
 app.listen(5000, () => {
